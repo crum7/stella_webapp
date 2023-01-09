9d47924477535a3f46e8fc0f73a0cfb9cc63721b
snowyowl <rikugamesion@gmail.com>
2023-01-09 21:54:32 +0900

    Update stella_webapp.py

--- stella_webapp.py
+++ stella_webapp.py
@@ -86,7 +86,7 @@ st.write('input: ', youtube_link)
 
 #テキストボックスが空じゃないとき
 if youtube_link!='':
-    output_file_path = '/app/stella_webapp/'+youtube_link[-5]
+    output_file_path = '/app/stella_webapp/'+youtube_link[-5:]
     ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl':  output_file_path + '.mp3',   # 出力先パス
@@ -101,23 +101,18 @@ if youtube_link!='':
     ydl = youtube_dl.YoutubeDL(ydl_opts)
     # 指定したパスに音声ファイルが格納される
     info_dict = ydl.extract_info(url, download=True)
-    st.write(info_dict)
 
     #mp3をwavに
-    with NamedTemporaryFile(dir='.', suffix='.mp3') as f:
-        f.write(uploaded_file.getbuffer())
-        video_file_path = f.name
-        st.write(f.name)
-        #ここで変換
-        sound = pydub.AudioSegment.from_mp3(video_file_path)
-        sound.export(video_file_path+".wav", format="wav")
-        #wav化して再生する
-        audio_file = open(video_file_path+'.wav', 'rb')
-        audio_bytes = audio_file.read()
-        st.audio(audio_bytes, format='audio/wav')
-        #取得したパスを基に音声認識をする
-        r = sr.Recognizer()
-        with sr.AudioFile(output_file_path + '.%(ext)s') as source2:
-            audio2 = r.record(source2)
-        text_from_video = r.recognize_google(audio2, language='ja-JP')
-        st.write(text_from_video)
+    #ここで変換
+    sound = pydub.AudioSegment.from_mp3(output_file_path + '.mp3')
+    sound.export(output_file_path+".wav", format="wav")
+    #wav化して再生する
+    audio_file = open(output_file_path+'.wav', 'rb')
+    audio_bytes = audio_file.read()
+    st.audio(audio_bytes, format='audio/wav')
+    #取得したパスを基に音声認識をする
+    r = sr.Recognizer()
+    with sr.AudioFile(output_file_path) as source2:
+        audio2 = r.record(source2)
+    text_from_video = r.recognize_google(audio2, language='ja-JP')
+    st.write(text_from_video)
