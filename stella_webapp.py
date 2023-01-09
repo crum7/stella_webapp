ebea880b42d2bd5d2e51f6181d6af2c6a34774b0
snowyowl <rikugamesion@gmail.com>
2023-01-09 20:24:57 +0900

    Update stella_webapp.py

--- stella_webapp.py
+++ stella_webapp.py
@@ -2,16 +2,25 @@ from tempfile import NamedTemporaryFile
 import streamlit as st
 import speech_recognition as sr
 import numpy
-
+import ffmpeg
 
 uploaded_file = st.file_uploader("File upload", type=['wav','mp4'])
 
 
 if uploaded_file:
+    st.write(uploaded_file.name)
+    if uploaded_file.name[:3] == 'mp4':
+        with NamedTemporaryFile(dir='.', suffix='.mp4') as f:
+            f.write(uploaded_file.getbuffer())
+            video_file_path = f.name
+            st.write(f.name)
+            stream = ffmpeg.input(video_file_path)
+            stream = ffmpeg.output(stream, video_file_path+'wav')
+            video_file_path = ffmpeg.run(stream)
 
 
 
-    
+
     #streamlitのuploaderは、ByteIOなのでそれを.wav形式に直す。
     #フルパスをvideo_file_pathに入れている
     with NamedTemporaryFile(dir='.', suffix='.wav') as f:
