a8cc844264ef13ac4166024d355cba32e9280eeb
snowyowl <rikugamesion@gmail.com>
2023-01-09 21:26:10 +0900

    Update stella_webapp.py

--- stella_webapp.py
+++ stella_webapp.py
@@ -9,21 +9,23 @@ uploaded_file = st.file_uploader("File upload", type=['wav','mp4','mp3'])
 
 if uploaded_file:
     st.write(uploaded_file.name)
-    #mp4をwavにかける
 
-    if str(uploaded_file.name)[-3:] == 'mp4':
+    #mp4→wav
+    if str(uploaded_file.name)[-3:] == 'mp4' or 'MOV':
         st.write('this is mp4 file')
         with NamedTemporaryFile(dir='.', suffix='.mp4') as f:
             f.write(uploaded_file.getbuffer())
             video_file_path = f.name
             st.write(video_file_path)
+
+            #ここで変換
             stream = ffmpeg.input(video_file_path)
             stream = ffmpeg.output(stream, video_file_path+'.wav')
             ffmpeg.run(stream,overwrite_output=True)
-
+            
+            #wav化して再生する
             audio_file = open(video_file_path+'.wav', 'rb')
             audio_bytes = audio_file.read()
-            #wav化して再生する
             st.audio(audio_bytes, format='audio/wav')
 
             #取得したパスを基に音声認識をする
@@ -33,7 +35,7 @@ if uploaded_file:
             text_from_video = r.recognize_google(audio2, language='ja-JP')
             st.write(text_from_video)
 
-
+    #mp3→wavにする
     elif str(uploaded_file.name)[-3:] == 'mp3':
         #streamlitのuploaderは、ByteIOなのでそれを.wav形式に直す。
         #フルパスをvideo_file_pathに入れている
@@ -42,10 +44,16 @@ if uploaded_file:
             video_file_path = f.name
             st.write(f.name)
 
+            #ここで変換
             sound = pydub.AudioSegment.from_mp3(video_file_path)
             sound.export(video_file_path+".wav", format="wav")
 
-                        #取得したパスを基に音声認識をする
+            #wav化して再生する
+            audio_file = open(video_file_path+'.wav', 'rb')
+            audio_bytes = audio_file.read()
+            st.audio(audio_bytes, format='audio/wav')
+
+            #取得したパスを基に音声認識をする
             r = sr.Recognizer()
             with sr.AudioFile(video_file_path+'.wav') as source2:
                 audio2 = r.record(source2)
