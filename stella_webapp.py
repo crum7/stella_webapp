2ce0310ef20449bc59bab6f0e801d80c458caf5b
snowyowl <rikugamesion@gmail.com>
2023-01-09 20:08:01 +0900

    Update stella_webapp.py

--- stella_webapp.py
+++ stella_webapp.py
@@ -9,9 +9,9 @@ if uploaded_file:
         video_file_path = f.name
         st.write(f.name)
 
-    #音声認識をする
-    r = sr.Recognizer()
-    with sr.AudioFile(video_file_path) as source2:
-        audio2 = r.record(source2)
-    text_from_video = r.recognize_google(audio2, language='ja-JP')
-    st.write(text_from_video)
+        #音声認識をする
+        r = sr.Recognizer()
+        with sr.AudioFile(video_file_path) as source2:
+            audio2 = r.record(source2)
+        text_from_video = r.recognize_google(audio2, language='ja-JP')
+        st.write(text_from_video)
