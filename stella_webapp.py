81497a9cc0f8df0c28733bc65d01076aa1857b02
snowyowl <rikugamesion@gmail.com>
2023-01-09 19:16:25 +0900

    Update stella_webapp.py

--- stella_webapp.py
+++ stella_webapp.py
@@ -3,8 +3,9 @@ import streamlit as st
 import pandas as pd
 
 uploaded_file = st.file_uploader("File upload", type='csv')
-with NamedTemporaryFile(dir='.', suffix='.csv') as f:
-    f.write(uploaded_file.getbuffer())
-    st.write(f.name)
-    df = pd.read_csv(f.name,encoding='shift_jis')
-    st.write(df)
+if uploaded_file:
+    with NamedTemporaryFile(dir='.', suffix='.csv') as f:
+        f.write(uploaded_file.getbuffer())
+        st.write(f.name)
+        df = pd.read_csv(f.name,encoding='shift_jis')
+        st.write(df)
