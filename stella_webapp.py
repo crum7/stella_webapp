248447e10c573005af4378a7220ca571c67e7f4a
snowyowl <rikugamesion@gmail.com>
2023-01-09 20:48:28 +0900

    Update stella_webapp.py

--- stella_webapp.py
+++ stella_webapp.py
@@ -21,23 +21,23 @@ if uploaded_file:
 
             #取得したパスを基に音声認識をする
             r = sr.Recognizer()
-            with sr.AudioFile(stream) as source2:
+            with sr.AudioFile(video_file_path) as source2:
                 audio2 = r.record(source2)
             text_from_video = r.recognize_google(audio2, language='ja-JP')
             st.write(text_from_video)
 
 
+    else:
+        #streamlitのuploaderは、ByteIOなのでそれを.wav形式に直す。
+        #フルパスをvideo_file_pathに入れている
+        with NamedTemporaryFile(dir='.', suffix='.wav') as f:
+            f.write(uploaded_file.getbuffer())
+            video_file_path = f.name
+            st.write(f.name)
 
-    #streamlitのuploaderは、ByteIOなのでそれを.wav形式に直す。
-    #フルパスをvideo_file_pathに入れている
-    with NamedTemporaryFile(dir='.', suffix='.wav') as f:
-        f.write(uploaded_file.getbuffer())
-        video_file_path = f.name
-        st.write(f.name)
-
-        #取得したパスを基に音声認識をする
-        r = sr.Recognizer()
-        with sr.AudioFile(video_file_path) as source2:
-            audio2 = r.record(source2)
-        text_from_video = r.recognize_google(audio2, language='ja-JP')
-        st.write(text_from_video)
+            #取得したパスを基に音声認識をする
+            r = sr.Recognizer()
+            with sr.AudioFile(video_file_path) as source2:
+                audio2 = r.record(source2)
+            text_from_video = r.recognize_google(audio2, language='ja-JP')
+            st.write(text_from_video)
