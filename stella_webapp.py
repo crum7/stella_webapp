8bafbf95441ade9af61813a7446c4a31bb00d026
snowyowl <rikugamesion@gmail.com>
2023-01-10 12:22:10 +0900

    Update stella_webapp.py

--- stella_webapp.py
+++ stella_webapp.py
@@ -70,8 +70,8 @@ def cut_wav(filename,time,duration):  # WAVファイルを刈り奪る　形を�
         if original_len-current >=180:
             end_cut = start*frames + frames
         elif original_len-current <=180:
-            end_cut = start*frames + original_len-current
-
+            end_cut = start*frames + int(original_len-current)
+        st.write('start_cut'+str(start_cut))
         st.write('end_cut'+str(end_cut))
         print(start_cut)
         print(end_cut)
