2142a3972b1ddb4575aec86c3c7a5abe2b591d10
snowyowl <rikugamesion@gmail.com>
2023-01-27 22:59:24 +0900

    first

--- speech_test.py
+++ speech_test.py
@@ -1,13 +1,19 @@
 import streamlit as st
 from audiorecorder import audiorecorder
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
 
 st.title("Audio Recorder")
 audio = audiorecorder("Click to record", "Recording...")
 
 if len(audio) > 0:
-    # To play audio in frontend:
-    st.audio(audio.tobytes())
+    #frontEnd
+    #st.audio(audio.tobytes())
     
-    # To save audio to a file:
-    wav_file = open("audio.mp3", "wb")
-    wav_file.write(audio.tobytes())
+        # wavデータの分割（無音部分で区切る）
+    chunks = split_on_silence(audio.tobytes(), min_silence_len=2000, silence_thresh=-40, keep_silence=600)
+
+    # 分割したデータ毎にファイルに出力
+    for i, chunk in enumerate(chunks):
+        chunk.export("output" + str(i) +".wav", format="wav")
+        st.audio("output" + str(i) +".wav")
