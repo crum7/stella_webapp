6a3655acf9ef8982bcf1588f5c6a352730105d61
snowyowl <rikugamesion@gmail.com>
2023-01-27 22:38:16 +0900

    first

--- speech_test.py
+++ speech_test.py
@@ -3,6 +3,12 @@ from audio_recorder_streamlit import audio_recorder
 
 audio_bytes = audio_recorder()
 speak_data_dict = {}
-
+stop_flag =0
 if audio_bytes:
-    st.audio(audio_bytes, format="audio/wav")
+    
+    # To save audio to a file:
+    wav_file = open("audio.wav", "wb")
+    wav_file.write(audio_bytes.tobytes())
+
+    st.audio("audio.wav", format="audio/wav")
+    
