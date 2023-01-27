09a46631877dcd68747af1df28057012feb9e516
snowyowl <rikugamesion@gmail.com>
2023-01-27 22:42:11 +0900

    first

--- speech_test.py
+++ speech_test.py
@@ -1,14 +1,13 @@
 import streamlit as st
-from audio_recorder_streamlit import audio_recorder
+from audiorecorder import audiorecorder
 
-audio_bytes = audio_recorder()
-speak_data_dict = {}
-stop_flag =0
-if audio_bytes:
+st.title("Audio Recorder")
+audio = audiorecorder("Click to record", "Recording...")
+
+if len(audio) > 0:
+    # To play audio in frontend:
+    st.audio(audio.tobytes())
     
     # To save audio to a file:
-    wav_file = open("audio.wav", "wb")
-    wav_file.write(audio_bytes.tobytes())
-
-    st.audio("audio.wav", format="audio/wav")
-    
+    wav_file = open("audio.mp3", "wb")
+    wav_file.write(audio.tobytes())
